8ad86f48e5297517df6a5df08f5cbfbac1222e97
Cristian Bastidas <cbastid.dr@gmail.com>
2022-10-16 22:21:00 -0300

    Ejercicio bucles

---
+++ bucles.js
@@ -0,0 +1,27 @@
+/*Imprime impares */
+for(let i=20; i>=2; i-=2) {
+    console.log(i);
+}
+/*Disminuir multiplos de 3 */
+for(let i=1;i<=100;i++){
+    if(i % 3 == 0){
+        console.log(i);
+    }
+}
+/*imprime secuencia */
+for(let i=4; i>=-3.5; i-=1.5) {
+    console.log(i);
+}
+/*suma numeros del 1 al 100 */
+let sum = 0;
+for(let i=1;i<=100;i++){
+    sum = sum + i;
+}
+console.log(sum)
+/*factorial */
+let producty = 1;
+for(let i=1;i<=12;i++){
+    producty = producty * i;
+}
+console.log(producty)
+
